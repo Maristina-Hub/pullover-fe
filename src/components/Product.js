6569a856d7450a70823949af4c30971118dcfe80
Maristina-Hub <maristyna@yahoo.com>
2021-09-30 01:57:35 +0100

    updated component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-import { ProductConsumer } from '../context'
+// import { ProductConsumer } from '../context'
 
 
 
 class Product extends Component {
     render() {
-    const { id, img, brand, price, inCart} = this.props.product
+    const { img, brand, price, inCart} = this.props.product
     return (
         <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
             <div className='card'>
